d297739a5174f41d23e3d340ccf4bc32f157f1f2
ShawnKim79 <madsyntst@gmail.com>
2020-07-16 00:14:07 +0900

    now work

--- Chapter2_Python/Round_1_Open_your_kitchin/MyKitchin.py
+++ Chapter2_Python/Round_1_Open_your_kitchin/MyKitchin.py
@@ -1,6 +1,8 @@
 def main():
-    print("Hello Owner!")
-    name = input("이름을 알려주세요")
+    print("안녕하세요 사장님!")
+    owner_name = input("사장님의 이름을 알려주세요")
+    
+    print(name + " 사장님 앞으로 멋진 ")
 
 if __name__ == '__main__':
     main()
